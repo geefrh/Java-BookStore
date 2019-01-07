0d82a8b8ab5cbef001f21580dbf3a547024a9e19
geefrh <g.b.c.algra@gmail.com>
2019-01-07 21:45:37 +0100

    updateBooks

--- src/main/java/com/pluralsight/BookDAO.java
+++ src/main/java/com/pluralsight/BookDAO.java
@@ -65,6 +65,30 @@ public class BookDAO {
         return listBook;
     }
 
+    public void updateBook(Book book) {
+
+        String sql = "UPDATE book SET title = ?, author = ?, price = ? WHERE id = ?";
+
+
+        try {
+
+            PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+            statement.setString(1, book.getTitle());
+            statement.setString(2, book.getAuthor());
+            statement.setFloat(3, book.getPrice());
+            statement.setFloat(4, book.getId());
+
+            statement.executeUpdate();
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
     public boolean insertBook(Book book) {
         String sql = "INSERT INTO book (title, author, price) VALUES (?, ?, ?)";
 
--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -58,7 +58,12 @@ public class ControllerServlet extends HttpServlet {
                 case "/delete":
                     deleteBook(request, response);
                     break;
-
+                case "/edit":
+                    showEditForm(request, response);
+                    break;
+                case "/update":
+                    updateBook(request, response);
+                    break;
                 default:
                     listBooks(request, response);
                     break;
@@ -110,9 +115,36 @@ public class ControllerServlet extends HttpServlet {
         bookDAO.deleteBook(id);
 
         response.sendRedirect("list");
+    }
 
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        Book existingBook = bookDAO.getBook(id);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
+        request.setAttribute("book", existingBook);
+        dispatcher.forward(request, response);
 
     }
+
+    private void updateBook(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String title = request.getParameter("booktitle");
+        String author = request.getParameter("bookauthor");
+        String price = request.getParameter("bookprice");
+
+        Book book = new Book(id, title, author, Float.parseFloat(price));
+
+        bookDAO.updateBook(book);
+
+        response.sendRedirect("list");
+
+    }
+
 
 
     /**
